5ed9d4553a7baeafa057612088501b14d3da33d3
ICMMAC05-7528 <icmmac05-7528@ICMMAC05-7528s-Mac-mini.local>
2022-08-24 10:23:04 -0400

    MyEmotion

---
+++ MyEmotion/MyEmotion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyEmotion
+//
+//  Created by ICMMAC05-7528 on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pacman: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func Button(_ sender: Any) {
+        if (pacman.image == UIImage(named: "pacman")){
+            pacman.image = UIImage(named: "fantasma")
+        }else {
+            pacman.image = UIImage(named: "pacman")
+        }
+    }
+    
+
+}
+
